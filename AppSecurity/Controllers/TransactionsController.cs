686bf370c40a20bf7dd61c4ab01fa6e10bf9082d
Goran Zuri <goran.zuri@gmail.com>
2016-06-18 13:34:20 +0200

    Init app, removed AntiForgeryTokens

---
+++ AppSecurity/Controllers/TransactionsController.cs
@@ -0,0 +1,24 @@
+﻿using AppSecurity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppSecurity.Controllers
+{
+    [Authorize]
+    public class TransactionsController : Controller
+    {
+        // GET: Transactions
+        public ActionResult Index()
+        {
+            using (ApplicationDbContext context = ApplicationDbContext.Create())
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
